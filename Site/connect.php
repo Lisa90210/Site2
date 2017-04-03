a99beea2675a30527063c932df4ac962254e1bb7
Dima <nazdimash@yandex.ru>
2017-04-03 21:21:16 +0300

    first files

---
+++ Site/connect.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ִלטענטי
+ * Date: 03.04.2017
+ * Time: 21:14
+ */
+echo "Hello, World!";
---
+++ Site/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ִלטענטי
+ * Date: 03.04.2017
+ * Time: 21:13
+ */
